72694a8bd2caa972deee52042ae244070ff11ed6
c-fujita <oor327@gmail.com>
2022-03-13 19:45:56 +0900

    add models

---
+++ app/Models/Article.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Mpdels;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Article extends Model
+{
+    //
+}
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            $table->string('user_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->timestamp('created_at')->useCurrent()->nullable();
+            $table->timestamp('updated_at')->useCurrent()->nullable();
         });
     }
 
